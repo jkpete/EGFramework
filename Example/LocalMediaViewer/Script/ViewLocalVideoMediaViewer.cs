4197e989780514d7b2fc7b999b5aba3e8726ac75
jkpete <1031139173@qq.com>
2025-04-27 17:38:07 +0800

    add localmedia viewer

---
+++ Example/LocalMediaViewer/Script/ViewLocalVideoMediaViewer.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+using EGFramework;
+
+public partial class ViewLocalVideoMediaViewer : Node,IEGFramework
+{
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        
+    }
+    public void RefreshList(){
+        
+    }
+}
