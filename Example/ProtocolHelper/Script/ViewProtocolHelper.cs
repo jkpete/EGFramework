02d2418fae06b68bed755452808fb71c1ee8830e
jkpete <1031139173@qq.com>
2025-05-13 17:35:51 +0800

    add protocol tools

---
+++ Example/ProtocolHelper/Script/ViewProtocolHelper.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class ViewProtocolHelper : Node
+{
+    
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _ExitTree()
+    {
+
+    }
+}
