86fb9565cdeb15c64b318cf7198f04b20c98203b
jkpete <1031139173@qq.com>
2025-05-19 17:32:57 +0800

    add script

--- Example/ProtocolHelper/Script/ViewProtocolHelper.cs
+++ Example/ProtocolHelper/Script/ViewProtocolHelper.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class ViewProtocolHelper : Node
 {
-    
+
     public override void _Ready()
     {
 
@@ -12,5 +12,15 @@ public partial class ViewProtocolHelper : Node
     public override void _ExitTree()
     {
 
+    }
+
+    public void RefreshConnectList()
+    {
+
+    }
+
+    public void RefreshProtocolList()
+    {
+        
     }
 }
---
+++ Example/ProtocolHelper/Script/ViewProtocolHelperTerminal.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class ViewProtocolHelperTerminal : RichTextLabel
+{
+    public override void _Ready()
+    {
+        base._Ready();
+    }
+
+    public void ClearArea()
+    {
+
+    }
+    public void RefreshProtocol()
+    {
+        
+    }
+
+}
